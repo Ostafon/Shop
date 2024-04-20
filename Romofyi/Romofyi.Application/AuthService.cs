ca07e73d6e4d2ae5da9f566ce6b301086b461a2e
alexandr.stegantov <alexandr.stegantov@isa.utm.md>
2024-04-20 16:09:52 +0300

    Implement auth service

---
+++ Romofyi/Romofyi.Application/AuthService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Romofyi.Application;
+
+namespace WebApplication1.Application
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public AuthService(ApplicationDbContext context, IPasswordHasher passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        public Customer AuthenticateUser(string username, string password)
+        {
+            var user = _context.Customers.FirstOrDefault(c => c.Username == username);
+            if (user != null && _passwordHasher.VerifyPassword(password, user.PasswordHash))
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public bool HasRequiredRole(Customer user, string role)
+        {
+            return user.Role.Equals(role, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public Customer RegisterUser(string username, string password)
+        {
+            var passwordHash = _passwordHasher.HashPassword(password);
+
+            var newUser = new Customer
+            {
+                Username = username,
+                PasswordHash = passwordHash,
+                Role = "User"
+            };
+
+            _context.Customers.Add(newUser);
+            _context.SaveChanges();
+
+            return newUser;
+        }
+    }
+}
