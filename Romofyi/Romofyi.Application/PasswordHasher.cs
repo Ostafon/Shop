8cbe02846e18050dffcd3ed66d269fe26ec8a244
sashaostafi60 <alexandr.stegantovsashaostafi60@gmail.com>
2024-04-21 11:23:16 +0300

    Add password hasher

---
+++ Romofyi/Romofyi.Application/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Romofyi.Application
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public bool VerifyPassword(string password, string storedHash)
+        {
+            var hash = HashPassword(password);
+            return hash == storedHash;
+        }
+    }
+}
